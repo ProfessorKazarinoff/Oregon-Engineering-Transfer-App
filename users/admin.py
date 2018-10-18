2068dd3edaf00632280ca37a94e6418ae6a24117
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-10-18 15:46:30 -0700

    added custom fields 'job' and 'university' to users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -11,6 +11,7 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    list_display = ['username','email','university','job','is_staff']
 
 
 admin.site.register(CustomUser,CustomUserAdmin)

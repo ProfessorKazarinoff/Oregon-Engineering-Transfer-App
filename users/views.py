5905b3e250e9ca733fa86a1ed03038373ab1e6ce
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-10-19 11:38:29 -0700

    Login button in upper right. Login or out with button. Button shows a drop down with user options when a user is logged in.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# from django.views.generic import DetailView
+
+
+# class ProfilePageView(DetailView):
+# template_name = 'users/profile.html'

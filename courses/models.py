ce0bba7d1e03f899ba6ef291cca18148f3af4555
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-10-24 15:40:54 -0700

    Added courses app and courses model. Added courses model to Django admin. Migrated courses model twice.

---
+++ courses/models.py
@@ -0,0 +1,27 @@
+# courses/models.py
+
+from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.urls import reverse
+
+
+class Course(models.Model):
+    course_number = models.CharField(max_length=10)
+    course_name = models.CharField(max_length=50)
+    credits = models.FloatField()
+    college = models.CharField(max_length=30)
+    department = models.CharField(max_length=100)
+    pre_reqs = models.CharField(max_length=50)
+    course_description = models.TextField()
+    course_outcomes = models.TextField()
+    course_URL = models.URLField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.course_number} {self.course_name} {self.college}'
+
+    def get_absolute_url(self):
+        return reverse('course_detail', args=[str(self.id)])
